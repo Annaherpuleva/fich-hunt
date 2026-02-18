5eee758e204b7ea783bd2948a95e3e8cfe030aef
Annaherpuleva <annaherpuleva@gmail.com>
2026-02-18 10:34:01 +0200

    Delete client directory

--- client/src/WalletProvider.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { TonWalletProvider } from './wallet/tonWallet';
-
-export default function WalletContextProvider({ children }: { children: React.ReactNode }) {
-  return <TonWalletProvider>{children}</TonWalletProvider>;
-}
--- client/src/wallet/useWalletAddress.ts
+++
@@ -1,5 +0,0 @@
-import { useTonAddress } from '@tonconnect/ui-react';
-
-export function useWalletAddress(): string {
-  return useTonAddress(false) || '';
-}
